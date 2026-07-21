832ebd9e9e1bef6bfcfdea424dfc2ba87830f3b1
gursalidhananjay31-source <gursalidhananjay31@gmail.com>
2026-07-21 04:52:53 +0000

    //

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum,avg;
+    cout<<"Enter two number";
+    cin>>num1;
+    cin>>num2;
+    sum=num1+num2;
+    avg=sum/2;
+    cout<<"sum of two number="<<sum<<"Average="<<avg;
+    return 0;
+}
